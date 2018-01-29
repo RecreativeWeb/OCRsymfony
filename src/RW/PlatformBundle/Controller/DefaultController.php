64c406e3698aafe88e8015c2ad58955d70bb76b6
RecreativeWeb <apoeydo@gmail.com>
2018-01-29 22:22:40 +0100

    creation du bundle PlatformBundle dans le namespace RW

---
+++ src/RW/PlatformBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RW\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('RWPlatformBundle:Default:index.html.twig');
+    }
+}
---
+++ src/RW/PlatformBundle/RWPlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RW\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RWPlatformBundle extends Bundle
+{
+}
