e5526ff5210bc6b6e8c2dd8a509ec161840f26b0
RecreativeWeb <apoeydo@gmail.com>
2018-02-02 14:54:26 +0100

    Mise en place des templates

--- src/RW/PlatformBundle/Controller/AdvertController.php
+++ src/RW/PlatformBundle/Controller/AdvertController.php
@@ -4,15 +4,39 @@ namespace RW\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller
 {
 
-  public function indexAction()
+  public function indexAction($page)
   {
-    $content = $this->get('templating')->render('RWPlatformBundle:Advert:index.html.twig');
-    
-    return new Response($content);
+
+    // Notre liste d'annonce en dur
+    $listAdverts = array(
+      array(
+        'title'   => 'Recherche développpeur Symfony',
+        'id'      => 1,
+        'author'  => 'Alexandre',
+        'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'title'   => 'Mission de webmaster',
+        'id'      => 2,
+        'author'  => 'Hugo',
+        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'title'   => 'Offre de stage webdesigner',
+        'id'      => 3,
+        'author'  => 'Mathieu',
+        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+        'date'    => new \Datetime())
+    );
+
+    return $this->render('RWPlatformBundle:Advert:index.html.twig', array(
+    'listAdverts' => $listAdverts
+    ));
   }
 
 
@@ -24,13 +48,56 @@ class AdvertController extends Controller
   // 
   public function viewAction($id)
   {
-    // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+    $advert = array(
+      'title'   => 'Recherche développpeur Symfony2',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime()
+    );
 
-    // on récupèrera depuis la base de données l'annonce correspondant à l'id $id
+    return $this->render('RWPlatformBundle:Advert:view.html.twig', array(
+      'advert' => $advert
+    ));
+  }
+
+
+
+
+  public function editAction($id, Request $request)
+  {
+    // ...
     
-    // Puis on passera l'annonce à la vue pour qu'elle puisse l'afficher
+    $advert = array(
+      'title'   => 'Recherche développpeur Symfony',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime()
+    );
 
-    return new Response("Affichage de l'annonce d'id : ".$id);
+    return $this->render('RWPlatformBundle:Advert:edit.html.twig', array(
+      'advert' => $advert
+    ));
+  }
+
+
+
+  public function menuAction($limit)
+  {
+    // On fixe en dur une liste ici, bien entendu par la suite
+    // on la récupérera depuis la BDD !
+    $listAdverts = array(
+      array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+      array('id' => 5, 'title' => 'Mission de webmaster'),
+      array('id' => 9, 'title' => 'Offre de stage webdesigner')
+    );
+
+    return $this->render('RWPlatformBundle:Advert:menu.html.twig', array(
+      // Tout l'intérêt est ici : le contrôleur passe
+      // les variables nécessaires au template !
+      'listAdverts' => $listAdverts
+    ));
   }
     
 
